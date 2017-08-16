575a2e726bdc401ce4a9b8c80c4aca0951588d30
Daniel Berger <dberger@redhat.com>
2017-08-16 09:22:17 -0500

    Switch to named params, update docs.

--- lib/attempt.rb
+++ lib/attempt.rb
@@ -6,7 +6,7 @@ require 'structured_warnings'
 class Attempt
 
   # The version of the attempt library.
-  VERSION = '0.3.2'.freeze
+  VERSION = '0.4.0'.freeze
 
   # Warning raised if an attempt fails before the maximum number of tries
   # has been reached.
@@ -38,21 +38,34 @@ class Attempt
   attr_accessor :level
 
   # :call-seq:
-  #    Attempt.new{ |a| ... }
+  #    Attempt.new(**kwargs)
   #
-  # Creates and returns a new +Attempt+ object.  Use a block to set the
-  # accessors.
+  # Creates and returns a new +Attempt+ object. The supported keyword options
+  # are as follows:
   #
-  def initialize
-    @tries     = 3         # Reasonable default
-    @interval  = 60        # Reasonable default
-    @log       = nil       # Should be an IO handle, if provided
-    @increment = nil       # Should be an int, if provided
-    @timeout   = nil       # Wrap the code in a timeout block if provided
-    @level     = Exception # Level of exception to be caught
-    @warnings  = true      # Errors are sent to STDERR as warnings if true
-
-    yield self if block_given?
+  # * tries     - The number of attempts to make before giving up. The default is 3.
+  # * interval  - The delay in seconds between each attempt. The default is 60.
+  # * log       - An IO handle where warnings/errors are logged to. The default is nil.
+  # * increment - The amount to increment the interval between tries. The default is 0.
+  # * level     - The level of exception to be caught. The default is everything, i.e. Exception.
+  # * warnings  - Boolean value that indicates whether or not errors are treated as warnings
+  #               until the maximum number of attempts has been made. The default is true.
+  # * timeout   - Boolean value to indicate whether or not to automatically wrap your
+  #               proc in a Timeout block. The default is false.
+  #
+  # Example:
+  #
+  #   a = Attempt.new(tries: 5, increment: 10, timeout: true)
+  #   a.attempt{ http.get("http://something.foo.com") }
+  #
+  def initialize(**options)
+    @tries     = options[:tries] || 3         # Reasonable default
+    @interval  = options[:interval] || 60     # Reasonable default
+    @log       = options[:log]                # Should be an IO handle, if provided
+    @increment = options[:increment] || 0     # Should be an integer, if provided
+    @timeout   = options[:timeout] || false   # Wrap the code in a timeout block if provided
+    @level     = options[:level] || Exception # Level of exception to be caught
+    @warnings  = options[:warnings] || true   # Errors are sent to STDERR as warnings if true
   end
 
   # Attempt to perform the operation in the provided block up to +tries+
@@ -87,34 +100,28 @@ end
 
 module Kernel
    # :call-seq:
-   #    attempt(tries = 3, interval = 60, timeout = nil){ # some op }
+   #    attempt(tries: 3, interval: 60, timeout: 10){ # some op }
    #
    # Attempt to perform the operation in the provided block up to +tries+
-   # times, sleeping +interval+ between each try.  By default the number
+   # times, sleeping +interval+ between each try. By default the number
    # of tries defaults to 3, the interval defaults to 60 seconds, and there
    # is no timeout specified.
    #
    # If +timeout+ is provided then the operation is wrapped in a Timeout
-   # block as well.  This is handy for those rare occasions when an IO
+   # block as well. This is handy for those rare occasions when an IO
    # connection could hang indefinitely, for example.
    #
    # If the operation still fails the (last) error is then re-raised.
    #
-   # This is really just a wrapper for Attempt.new where the simple case is
-   # good enough i.e. you don't care about warnings, increments or logging,
-   # and you want a little added convenience.
+   # This is really just a convenient wrapper for Attempt.new + Attempt#attempt.
    #
    # Example:
    #
    #    # Make 3 attempts to connect to the database, 60 seconds apart.
    #    attempt{ DBI.connect(dsn, user, passwd) }
    #
-   def attempt(tries = 3, interval = 60, timeout = nil, &block)
-      raise 'no block given' unless block_given?
-      Attempt.new{ |a|
-        a.tries    = tries
-        a.interval = interval
-        a.timeout  = timeout if timeout
-      }.attempt(&block)
+   def attempt(**kwargs)
+     object = Attempt.new(kwargs)
+     object.attempt(&block)
    end
 end
